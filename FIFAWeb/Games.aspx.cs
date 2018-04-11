9f519398669084dd05aaaf176d6a70ed0b823abe
IÑAKI CABO ALONSO <icaboalo@itam.mx>
2018-04-11 12:57:07 -0500

    starting creating team

--- FIFAWeb/Games.aspx.cs
+++ FIFAWeb/Games.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Odbc;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -7,8 +8,30 @@ using System.Web.UI.WebControls;
 
 public partial class Games : System.Web.UI.Page
 {
+    protected OdbcConnection addConnection()
+    {
+        try
+        {
+            OdbcConnection con = new OdbcConnection("Driver={SQL Server Native Client 11.0};Server=localhost;Database=fifa;Trusted_Connection={Yes};");
+            con.Open();
+            return con;
+        }
+        catch (Exception e)
+        {
+            return null;
+        }
+    }
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void ddTeam_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        gvPast.DataSource = Match.getPreviousMatches(ddTeam.SelectedValue, addConnection());
+        gvPast.DataBind();
+
+        gvNext.DataSource = Match.getNextMatches(ddTeam.SelectedValue, addConnection());
+        gvNext.DataBind();
     }
 }
